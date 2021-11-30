f180df916cecd9195ee5c52355cc876b21383add
jake <rothjakem@gmail.com>
2021-11-30 00:48:37 -0600

    comment on f/t vs ff/tt and reorder in lines parsing

--- src/parsers/OPF2PD.jl
+++ src/parsers/OPF2PD.jl
@@ -62,7 +62,7 @@ function opf2pd(fileout::String, optimal_values::Dict, opfmodeldata::Dict, phys:
     L["params"]["to"] = "bus" * string(tt)
     L["params"]["Y"] = Dict()
     L["params"]["Y"]["re"] = 0.0
-    L["params"]["Y"]["im"] = opfmodeldata[:Y][f, t]
+    L["params"]["Y"]["im"] = opfmodeldata[:Y][ff, tt] # or should it be [f,t]?
     L["type"] = "StaticLine"
     # L["params"]["y"]["re"] = 0.0
     # L["params"]["y"]["im"] = opfmodeldata[:Y][f, t]
