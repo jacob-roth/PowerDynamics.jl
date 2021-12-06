e6844f008c793e0aae8c0040cdcd040a6d680e4b
jake <rothjakem@gmail.com>
2021-12-05 20:28:22 -0600

    notapnoshunt and notapyeshunt examples

---
+++ examples/118busnotapnoshunt/testnotapnoshunt.jl
@@ -0,0 +1,37 @@
+include("../../src/PowerDynamics.jl") # comment out `module` part
+using MPCCases
+include("/Users/jakeroth/git/OPF/src/OPF.jl") # comment out `module` part; this will ERROR, but ok
+include("/Users/jakeroth/git/OPF/src/OPF2PD.jl") # comment out `module` part
+include("/Users/jakeroth/git/OPF/src/util.jl") # comment out `module` part
+include("/Users/jakeroth/git/OPF/src/util_coupled.jl") # comment out `module` part
+include("/Users/jakeroth/git/OPF/src/powergrid_utils.jl") # comment out `module` part
+include("/Users/jakeroth/git/OPF/src/pfe.jl") # comment out `module` part
+using JSON
+using PowerDynamics: read_powergrid, Json, Inc, find_operationpoint, ChangeInitialConditions, LineFailure, PowerPerturbation, simulate
+
+##
+## 118bus with swingeq
+##
+
+powergrid = read_powergrid(joinpath(@__DIR__, "118notapnoshunt.json"), Json)
+operationpoint = find_operationpoint(powergrid, sol_method = :dynamic) # solve_powerflow=true, 
+
+operatingdata_path = "/Users/jakeroth/git/PowerDynamics.jl/examples/118busnotapnoshunt/"
+imported_vec = import_own_operatingpoint(powergrid, operatingdata_path)
+imported_operationpoint = State(powergrid, imported_vec)
+norm(imported_operationpoint.vec - operationpoint.vec)
+
+norm(err_from_root(imported_operationpoint)) # 1.0682947627257781e-6
+norm(err_from_root(operationpoint)) # 9.794761129874436e-9
+
+timespan = (0.0, 25) # make sure that tspan_fault has end time >= end time here
+include("plotexample_jr.jl")
+
+# simulating a frequency perturbation at node 1
+fault1 = ChangeInitialConditions(node = "bus1", var = :ω, f = Inc(0.0));
+solution1 = simulate(fault1, powergrid, operationpoint, timespan);
+plot1 = create_plot(solution1)
+
+fault1 = ChangeInitialConditions(node = "bus1", var = :ω, f = Inc(0.0));
+solution1 = simulate(fault1, powergrid, imported_operationpoint, timespan);
+plot1 = create_plot(solution1)
