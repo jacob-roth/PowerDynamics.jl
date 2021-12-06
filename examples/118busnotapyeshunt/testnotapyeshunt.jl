fd031557459b9b0a07ac38cd61f623cf3d9bdf06
jake <rothjakem@gmail.com>
2021-12-06 10:12:45 -0600

    notap yeshunt allow neg Bshunt

--- examples/118busnotapyeshunt/testnotapyeshunt.jl
+++ examples/118busnotapyeshunt/testnotapyeshunt.jl
@@ -1,6 +1,6 @@
 include("../../src/PowerDynamics.jl") # comment out `module` part
 using MPCCases
-include("/Users/jakeroth/git/OPF/src/OPF.jl") # comment out `module` part; this will ERROR, but ok
+include("/Users/jakeroth/git/OPF/src/OPF.jl") # comment out `module` part; this will ERROR @constraintref, but ok
 include("/Users/jakeroth/git/OPF/src/OPF2PD.jl") # comment out `module` part
 include("/Users/jakeroth/git/OPF/src/util.jl") # comment out `module` part
 include("/Users/jakeroth/git/OPF/src/util_coupled.jl") # comment out `module` part
@@ -14,15 +14,19 @@ using PowerDynamics: read_powergrid, Json, Inc, find_operationpoint, ChangeIniti
 ##
 
 powergrid = read_powergrid(joinpath(@__DIR__, "118notapyeshunt.json"), Json)
-operationpoint = find_operationpoint(powergrid, sol_method = :dynamic) # solve_powerflow=true, 
+# operationpoint = find_operationpoint(powergrid, sol_method = :dynamic) # solve_powerflow=true, 
 
 operatingdata_path = "/Users/jakeroth/git/PowerDynamics.jl/examples/118busnotapyeshunt/"
 imported_vec = import_own_operatingpoint(powergrid, operatingdata_path)
 imported_operationpoint = State(powergrid, imported_vec)
-norm(imported_operationpoint.vec - operationpoint.vec)
+# norm(imported_operationpoint.vec - operationpoint.vec)
 
 norm(err_from_root(imported_operationpoint)) # 2.942147413731256
-norm(err_from_root(operationpoint)) # 2.2147484453872968e-8
+# norm(err_from_root(operationpoint)) # 2.2147484453872968e-8
+
+bad_idx = findall(abs.(err_from_root(imported_operationpoint)) .> 1e-4)
+[vecidx2busvar(i,imported_operationpoint) for i in bad_idx]
+
 
 timespan = (0.0, 25) # make sure that tspan_fault has end time >= end time here
 include("plotexample_jr.jl")
