3c9d2070612f6b7f1338dbf9069a46c4366c7a47
jake <rothjakem@gmail.com>
2022-01-27 14:07:59 -0600

    rerun stats for IV.D.3 paragraph 2

--- VALIDATION/validation_swing_revised.jl
+++ VALIDATION/validation_swing_revised.jl
@@ -1,4 +1,6 @@
-import Pkg; Pkg.activate("."); Pkg.instantiate()
+import Pkg;
+Pkg.activate(".");
+Pkg.instantiate();
 using JuMP, Ipopt
 using MPCCases
 using JSON
@@ -13,7 +15,7 @@ include("/Users/jakeroth/git/OPF/src/pfe.jl") # comment out `module` part
 include("/Users/jakeroth/git/OPF/src/powerfloweq.jl")
 include("/Users/jakeroth/git/OPF/src/default.jl")
 include("/Users/jakeroth/git/OPF/src/util.jl") # comment out `module` part
-using PowerDynamics: read_powergrid, Json, Inc, find_operationpoint, ChangeInitialConditions, LineFailure, PowerPerturbation, simulate
+# using PowerDynamics: read_powergrid, Json, Inc, find_operationpoint, ChangeInitialConditions, LineFailure, PowerPerturbation, simulate
 
 include("opt.jl")
 include("pd.jl")
@@ -23,21 +25,21 @@ include("plot.jl")
 ## get case
 ##
 case_options_base = DefaultOptions();
-case_options_base[:lossless]           = true;
-case_options_base[:remove_tap]         = true;
-case_options_base[:remove_Bshunt]      = false;
-case_options_base[:solveOPF]           = false;
+case_options_base[:lossless] = true;
+case_options_base[:remove_tap] = true;
+case_options_base[:remove_Bshunt] = false;
+case_options_base[:solveOPF] = false;
 case_options_base[:constr_limit_scale] = 1.05;
-case_options_base[:current_rating]     = true;
-case_options_base[:op_model]           = :acopf;
-case_options_base[:shed_load]          = false;
+case_options_base[:current_rating] = true;
+case_options_base[:op_model] = :acopf;
+case_options_base[:shed_load] = false;
 case_path = "/Users/jakeroth/Desktop/planning-large-deviation/data/cases/118-files/"
 case_name = "mpc_lowdamp_pgliblimits"
-casedata = load_case(case_name, case_path; other=true);
-opfdata  = casedata.opf;
+casedata = load_case(case_name, case_path; other = true);
+opfdata = casedata.opf;
 case_options = deepcopy(case_options_base);
-opfdata   = casedata.opf;
-physdata  = casedata.phys;
+opfdata = casedata.opf;
+physdata = casedata.phys;
 phys = physDefault;
 case_options[:remove_Bshunt] = false
 case_options[:nonneg_Bshunt] = false
@@ -63,7 +65,7 @@ powergrid0 = read_powergrid(grid_file, Json)
 
 op_kmc_dict, status = simple_energy_model(opfmodeldata, optimal_values)
 oklinenames, oklinemask = get_ok_lines(powergrid0)
-oklines = parse.(Int,[replace(k,"branch" => "" ) for k in oklinenames])
+oklines = parse.(Int, [replace(k, "branch" => "") for k in oklinenames])
 op_kmc_dict_ls = []
 status_ls = []
 for l in oklines
@@ -85,7 +87,7 @@ op_kmc_vec = import_own_operatingpoint(powergrid0, op_kmc_dict)
 op_kmc = State(powergrid0, op_kmc_vec)
 @assert(norm(err_from_root(op_kmc)) < 5e-5)
 
-op_pd = find_operationpoint(powergrid0, sol_method=:dynamic)
+op_pd = find_operationpoint(powergrid0, sol_method = :dynamic)
 @assert(norm(err_from_root(op_pd)) < 5e-5)
 
 global op_kmc2pds = Array{Any}(undef, length(oklines))
@@ -93,25 +95,28 @@ global tstable_op_kmc2pds = Array{Any}(undef, length(oklines))
 tol = 1e-5
 for l in oklines
     println("line=$l, idx=$(id2idx[l])")
-    fault_l = LineFailure(line_name="branch$(l)", tspan_fault=(1,Inf))
+    fault_l = LineFailure(line_name = "branch$(l)", tspan_fault = (1, Inf))
     try
-        global sol_kmc2pd = simulate(fault_l, powergrid0, op_kmc, (0,1e3));
+        global sol_kmc2pd = simulate(fault_l, powergrid0, op_kmc, (0, 1e3))
         global tstable_op_kmc2pd = find_stable_time(sol_kmc2pd, powergrid0, fault_l, tol)
-        global op_kmc2pds[id2idx[l]] = sol_kmc2pd.dqsol[end];
-        global tstable_op_kmc2pds[id2idx[l]] = tstable_op_kmc2pd;
+        global op_kmc2pds[id2idx[l]] = sol_kmc2pd.dqsol[end]
+        global tstable_op_kmc2pds[id2idx[l]] = tstable_op_kmc2pd
     catch e
         println(e)
         global sol_pd = -Inf
         global tstable_op_pd = -Inf
     finally
-        global op_kmc2pds[id2idx[l]] = sol_kmc2pd.dqsol[end];
-        global tstable_op_kmc2pds[id2idx[l]] = tstable_op_kmc2pd;
+        global op_kmc2pds[id2idx[l]] = sol_kmc2pd.dqsol[end]
+        global tstable_op_kmc2pds[id2idx[l]] = tstable_op_kmc2pd
     end
 end
 
 writedlm("data/op_kmc2pds_swing.csv", op_kmc2pds)
 writedlm("data/tstable_op_kmc2pds_swing.csv", tstable_op_kmc2pds)
 
+##
+## compare operating points
+##
 
 op_kmc2pds = readdlm("data/op_kmc2pds_swing.csv")
 tstable_op_kmc2pds = readdlm("data/tstable_op_kmc2pds_swing.csv")
@@ -125,53 +130,62 @@ op_diffs = []
 for l in oklines
     idx = id2idx[l]
     println("line $l")
-    fault_l = LineFailure(line_name="branch$(l)", tspan_fault=(1,Inf))
+    fault_l = LineFailure(line_name = "branch$(l)", tspan_fault = (1, Inf))
     pg = deepcopy(pg0)
     grid_l = fault_l(pg)
-    kmc2pd_v = op_kmc2pds[idx,:]
-    kmc2kmc_v = import_own_operatingpoint(grid_l, op_kmc_dict_ls[idx])
+    kmc2pd_v = op_kmc2pds[idx, :] # solve PD from line failures for op point
+    kmc2kmc_v = import_own_operatingpoint(grid_l, op_kmc_dict_ls[idx]) # solve EM (energy model) for line failures (as per kmc) for op point
     kmc2kmc_state = State(grid_l, kmc2kmc_v)
     kmc2pd_state = State(grid_l, kmc2pd_v)
 
     err_kmc2kmc_vec = err_from_root(kmc2kmc_state)
     err_kmc2pd_vec = err_from_root(kmc2pd_state)
     err_kmc2kmc = norm(err_kmc2kmc_vec)
-    err_kmc2pd = norm( err_kmc2pd_vec)
-    push!(op_stability_kmc2kmc, err_kmc2kmc)
-    push!(op_stability_kmc2pd, err_kmc2pd)
-    push!(op_diff, norm(kmc2kmc_v - kmc2pd_v))
+    err_kmc2pd = norm(err_kmc2pd_vec)
+    push!(op_stability_kmc2kmc, err_kmc2kmc) # is the point solved by EM PD-stable?
+    push!(op_stability_kmc2pd, err_kmc2pd) # is the point solved by PD PD-stable?
+    push!(op_diffs, norm(kmc2kmc_v - kmc2pd_v)) # are the points the same?
 end
 
 writedlm("data/op_stability_kmc2pd_swing.csv", op_stability_kmc2pd)
 writedlm("data/op_stability_kmc2kmc_swing.csv", op_stability_kmc2kmc)
 
-
 ##
-## stats
+## stats: IV.D.3 paragraph 2
 ##
-maximum(tstable_op_kmc2pds)
-mean(tstable_op_kmc2pds[tstable_op_kmc2pds.>0])
-std(tstable_op_kmc2pds[tstable_op_kmc2pds.>0])
 
-powergrid = powergrid0
-PD_line_names = collect(keys(powergrid.lines))
-PD_line_IDs = parse.(Int,[replace(k,"branch" => "" ) for k in PD_line_names])
+# energy model
+stable_lines_em = oklines[abs.(vec(op_stability_kmc2kmc)).<1e-5] # 127 lines
+unstable_lines_em = oklines[abs.(vec(op_stability_kmc2kmc)).>1e-5] # 7 lines
 
-err_IDs = findall(vec(tstable_op_kmc2pds).<0)
-nonerr_IDs = findall(vec(tstable_op_kmc2pds).>0)
-stable_line_IDs = nonerr_IDs
-unstable_line_IDs = err_IDs
+# power dynamics
+stable_lines_pd = oklines[vec(tstable_op_kmc2pds).>0] # 129 lines
+unstable_lines_pd = oklines[vec(tstable_op_kmc2pds).<0] # 5 lines
 
-## pd totals
-n_ok_PD_lines = length(oklines)
-n_stable_lines = length(stable_line_IDs)
+# both
+stable_lines_both = oklines[(abs.(vec(op_stability_kmc2kmc)).<1e-5).*(vec(tstable_op_kmc2pds).>0)] # 126 lines
+unstable_lines_both = oklines[(abs.(vec(op_stability_kmc2kmc)).>1e-5).*(vec(tstable_op_kmc2pds).<0)] # 4 lines
+stable_em_unstable_pd = oklines[(abs.(vec(op_stability_kmc2kmc)).<1e-5).*(vec(tstable_op_kmc2pds).<0)] # 1 lines
+unstable_em_stable_pd = oklines[(abs.(vec(op_stability_kmc2kmc)).>1e-5).*(vec(tstable_op_kmc2pds).>0)] # 3 lines
 
-## kmc stable/unstable for PD stable
-n_KMC_unstable_PD_stable = sum(abs.(op_stability_kmc2kmc[stable_line_IDs]).>1e-5)
-n_KMC_stable_PD_stable = sum(abs.(op_stability_kmc2kmc[stable_line_IDs]).<=1e-5)
+# true/false postive/negative rates
+em_tpr_stable = length(stable_lines_both) / length(stable_lines_pd) # 98% = 126 / 129
+em_fnr_stable = length(unstable_em_stable_pd) / length(stable_lines_pd) # 2% = 3 / 129
+em_fpr_unstable = length(stable_em_unstable_pd) / length(unstable_lines_pd) # 20% = 1 / 5
+em_tpr_unstable = length(unstable_lines_both) / length(unstable_lines_pd) # 80% = 4 / 5
 
-## kmc stable/unstable for PD unstable
-n_KMC_stable_PD_unstable = sum(abs.(op_stability_kmc2kmc[unstable_line_IDs]).>1e-5)
-n_KMC_unstable_PD_unstable = sum(abs.(op_stability_kmc2kmc[unstable_line_IDs]).<=1e-5)
+# overall stability pct
+stable_overall_pct_pd = length(stable_lines_pd) / length(oklines) # 96% = 129 / 134; PD-stable
+stable_overall_pct_em = length(stable_lines_em) / length(oklines) # 95% = 127 / 134; EM-stable
 
-findall(abs.(op_diff).>1e-5)
+# timing stats (for PD-stable)
+maximum(tstable_op_kmc2pds) # 34.6s
+mean(tstable_op_kmc2pds[tstable_op_kmc2pds.>0]) # 27.4s
+std(tstable_op_kmc2pds[tstable_op_kmc2pds.>0]) # 3.1s
+
+# when didn't they find the same operating point?
+mask = abs.(op_diffs) .> 1e-5
+diff_lines__stable_em_stable_pd = oklines[mask.*(abs.(vec(op_stability_kmc2kmc)).<1e-5).*(vec(tstable_op_kmc2pds).>0)]
+diff_lines__unstable_em_stable_pd = oklines[mask.*(abs.(vec(op_stability_kmc2kmc)).>1e-5).*(vec(tstable_op_kmc2pds).>0)]
+diff_lines__stable_em_unstable_pd = oklines[mask.*(abs.(vec(op_stability_kmc2kmc)).<1e-5).*(vec(tstable_op_kmc2pds).<0)]
+diff_lines__unstable_em_unstable_pd = oklines[mask.*(abs.(vec(op_stability_kmc2kmc)).>1e-5).*(vec(tstable_op_kmc2pds).<0)]
